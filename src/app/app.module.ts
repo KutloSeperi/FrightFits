42bf7987a39b675ed3d05eb9444d82df368d0e18
[Kutlo Seperi] <[kutlocoopseperi@gmail.com]>
2025-03-13 02:24:43 +0200

    {commit_message}

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ShopComponent,
+    CartComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
